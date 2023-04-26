f0436eb6f856b88570327660cc5ae453050b99b2
Gael Ramos <gaelramosrr@gmail.com>
2023-04-25 20:40:35 -0500

    The addition of the withdraw function

--- main.py
+++ main.py
@@ -9,6 +9,26 @@ connection = mysql.connector.connect(
     database = "example", 
     password = "Megatro102030#")
 
+def withdraw(amount,number):    
+    curso = connection.cursor()    
+    
+    amount = int(amount)
+    number = int(number)
+    curso.execute("SELECT balance FROM bank_account WHERE account_number = %s", (number,))   
+
+    b_balance = curso.fetchall()        
+
+    print("Your before balance is: "+ str(b_balance)+" .")      
+  
+    
+    curso.execute("UPDATE bank_account SET balance = balance - %s  WHERE account_number = %s", (amount,number))
+
+    
+    curso.execute("SELECT balance FROM bank_account WHERE account_number = %s", (number,))
+    b_balance = curso.fetchone()[0] 
+
+    print("Your after balance is: "+str(b_balance)+" .")
+    connection.commit()
  
 cursorr = connection.cursor()
 cursorr.execute("SELECT account_number, pin FROM bank_account")
@@ -28,7 +48,10 @@ while i == 0 :
                 i += 2
             else:
                 i == 0
-                
+
+m = 0            
+while m == 0:
+    print("Hello")
 cursorr.close()
 
 connection.close()
